f41079245b18d63d93c73d00a2a927cd699bd85e
AnRandomSilicium <167401748+AnRandomSilicium@users.noreply.github.com>
2024-04-22 17:14:45 +0800

    ap.js

--- ap.js
+++ ap.js
@@ -10,14 +10,19 @@ app.use("/static", function(request, response){
     response.send(`<h1>Hello</h1><p>Octagon NodeJS Test</p>`);
 
 });
-app.use("/dynamic", function(request, response){
+app.get('/dynamic', (request, response) => {
+    const a = parseFloat(request.query.a);
+    const b = parseFloat(request.query.b);
+    const c = parseFloat(request.query.c);
 
-    const a = 1;
-    const b= 2;
-    const c= 3; 
-    const d= (a+b+c)/3;
-    response.send(`<h1>Calculated</h1><p> ${d} </p>`);
+    if (isNaN(a),  isNaN(b),  isNaN(c)) {
+        response.send("<h1>Error</h1>");
+    } else {
+        const d = (a * b * c) / 3;
+        response.send(`<h1><strong>  Calculated </strong></h1> ${d}`)
+    }
 });
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
