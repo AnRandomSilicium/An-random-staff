f4c61b22cf44554e5f5910d2579299212a72676f
AnRandomSilicium <167401748+AnRandomSilicium@users.noreply.github.com>
2024-05-04 14:51:52 +0800

    ap.js

--- ap.js
+++ ap.js
@@ -17,12 +17,12 @@ app.get('/dynamic', (request, response) => {
         if (!(request.query[variable] != null&& !isNaN(parseFloat(request.query[variable])))) {
             response.send("<h1>Error</h1>");
             return;
+        
         }
-else {
-    const result = (parseFloat(request.query.a) * parseFloat(request.query.b) * parseFloat(request.query.c)) / 3;
-    response.send(`<h1><strong>Calculated</strong></h1> <p>${result}</p>`);
+
     }
-    }
+        const result = (parseFloat(request.query.a) * parseFloat(request.query.b) * parseFloat(request.query.c)) / 3;
+        response.send(`<h1><strong>Calculated</strong></h1> <p>${result}</p>`);
 });
 
 app.listen(3000, () => {
